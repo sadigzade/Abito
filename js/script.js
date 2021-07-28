5731055a56cc09f5bd02efbb031b8f6e2de781bd
Sadigzade Hikmet <sadigzade.hi@mail.ru>
2021-07-28 11:22:29 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function testWebP(callback) {
+  var webP = new Image();
+  webP.onload = webP.onerror = function () {
+    callback(webP.height == 2);
+  };
+  webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+  if (support == true) {
+    document.querySelector('body').classList.add('webp');
+  } else {
+    document.querySelector('body').classList.add('no-webp');
+  }
+});;
+$(document).ready(function () {
+  $('.menu__burger').click(function (event) {
+    $('.menu__burger, .menu__body').toggleClass('_active');
+    $('.header').toggleClass('_special');
+  });
+});;
