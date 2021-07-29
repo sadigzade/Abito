9b3f82169532f5981cb8c0566317a273034977fa
Sadigzade Hikmet <sadigzade.hi@mail.ru>
2021-07-29 12:02:22 +0300

    Add page -single-

--- js/script.js
+++ js/script.js
@@ -19,3 +19,16 @@ $(document).ready(function () {
     $('.header').toggleClass('_special');
   });
 });;
+const swiper = new Swiper(".mySwiper", {
+  spaceBetween: 10,
+  slidesPerView: 5,
+  freeMode: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+});
+const swiper2 = new Swiper(".mySwiper2", {
+  spaceBetween: 10,
+  thumbs: {
+    swiper: swiper,
+  },
+});;
